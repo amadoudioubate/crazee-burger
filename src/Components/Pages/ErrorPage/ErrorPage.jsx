0102ddac83c0ae20581f9471b903f5a382187208
Amadou Dioubate <dioubateamadou@yahoo.fr>
2023-08-11 00:57:12 +0200

    ♻️ f02: ERRORPAGE > Replace useNavigate with Link

--- src/Components/Pages/ErrorPage/ErrorPage.jsx
+++ src/Components/Pages/ErrorPage/ErrorPage.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default function ErrorPage() {
-    // State
-    const navigate = useNavigate()
-    
-    // Set behavior
-    const handleBack = () => navigate("/")
 
     // Display (render)
   return (
     <div>
         <h2>ErrorPage</h2>
-        <button type='button' onClick={handleBack}>Retour à la page d'acceuil</button>
+        <Link to='/'>
+          <button type='button'>Retourner à la page d'acceuil</button>
+        </Link>  
     </div>
   )
 }
